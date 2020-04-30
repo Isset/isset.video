0ee93ef51b56445248c9f9d1bfdd4e0737d30543
Nils <nils@isset.nl>
2020-04-30 15:28:29 +0200

    More renaming

--- plugin/isset-video-publisher.php
+++ plugin/isset-video-publisher.php
@@ -7,7 +7,7 @@ Description: Wordpress isset.video implementation
 Version: 0.1.0
 Requires at least: 5.0
 Required PHP: 7.3
-Author: Isset B.V.
+Author: isset.video
 Author URI: http://isset.nl/
 License: GPL v3
 License URI: https://www.gnu.org/licenses/gpl-3.0.en.html
