a8d217ec54f376131b94197c2209f710ac19c6d7
Sjon Deux <sjon@isset.nl>
2021-04-28 16:41:19 +0200

    - Added custom stills

--- js/overview/components/admin-video-details.js
+++ js/overview/components/admin-video-details.js
@@ -7,6 +7,8 @@ import filesize from '../helpers/filesize';
 import AdminCopyText from './admin-copy-text';
 import PropTypes from 'prop-types';
 import {__} from '@wordpress/i18n';
+import {UPLOAD_ADDED} from '../upload/uploadStatuses';
+import {uploadCustomStill} from '../upload/api';
 
 class AdminVideoDetails extends React.Component {
     static propTypes = {
@@ -30,6 +32,8 @@ class AdminVideoDetails extends React.Component {
             assets: [],
             defaultImage: null,
             playerUrl: '',
+            settingCustomImage: false,
+            files: [],
         }
     }
 
@@ -80,8 +84,25 @@ class AdminVideoDetails extends React.Component {
                 playerUrl: player_url,
                 assets,
                 defaultImage: this.findDefaultImage(assets),
+                settingCustomImage: false,
+                files: [],
             });
         }).catch(err => console.log(err));
+    };
+
+    filesAdded = (event) => {
+        const {target: {files}} = event;
+        const selectedFiles = [];
+
+        for (let i = 0; i < files.length; i++) {
+            let file = files[i];
+
+            file.progress = 0;
+            file.status = UPLOAD_ADDED;
+            selectedFiles.push(file);
+        }
+
+        this.setState({files: [...selectedFiles]});
     };
 
     findDefaultImage(assets) {
@@ -98,6 +119,62 @@ class AdminVideoDetails extends React.Component {
         }).catch(err => console.log(err));
     };
 
+    hasCustomImage = assets => assets.filter(asset => asset.id === 0).length > 0;
+    uploadCustomImage = () => {
+        this.setState({settingCustomImage: true});
+    };
+
+    doUpload = () => {
+        const {uuid, files} = this.state;
+
+        if (files.length === 1) {
+            const result = uploadCustomStill(uuid, files[0]);
+
+            result.then(() => this.loadPublish());
+        }
+    };
+
+    closeCustomStillDialog = () => {
+        this.setState({settingCustomImage: false, files: []});
+    };
+
+    renderFilenames = files => {
+        if (files.length === 1) {
+            return <div className="text-wrapper">
+                Uploading file: {files[0].name}
+            </div>;
+        }
+
+        return <>
+            <div className="text-wrapper">
+                <span className="dashicons dashicons-download download-icon" />
+            </div>
+            <div className="text-wrapper">
+                <p>{__('Drop an image file here or browse your computer.', 'isset-video')}</p>
+            </div>
+        </>;
+    };
+
+    renderUploadCustomStill = () => {
+        const {files} = this.state;
+
+        return <div className="isset-video-overlay video-publisher-admin">
+            <div className="isset-video-custom-still-dialog">
+                <h2 className="video-publisher-text-white">Upload Custom Still</h2>
+                <div className="phase-select isset-video-image-upload">
+                    <div className="phase-select-dropzone">
+                        <input accept=".jpg,.jpeg,.png" type="file" onChange={this.filesAdded} />
+                        <div className="selected-image-container video-publisher-p-2">
+                            {this.renderFilenames(files)}
+                        </div>
+                    </div>
+                    {files.length > 0 ? <button className="isset-video-btn video-publisher-mr-2" onClick={this.doUpload}>{__('Upload', 'isset-video')}</button> : ''}
+                    <button className="isset-video-btn btn-danger" onClick={this.closeCustomStillDialog}>{__('Cancel', 'isset-video')}</button>
+                </div>
+            </div>
+        </div>;
+    };
+
     renderDetails = () => {
         const {videoName, uuidParsed, assets, playerUrl, defaultImage, publish: {presets}} = this.state;
         const {file: {
@@ -106,6 +183,7 @@ class AdminVideoDetails extends React.Component {
             height,
             size,
             width,
+            settingCustomImage,
         }} = this.state;
 
         if (!playerUrl) {
@@ -129,7 +207,13 @@ class AdminVideoDetails extends React.Component {
                     <AdminCopyText text={uuidParsed} />
 
                     <div className="isset-video-stills-container">
-                        {assets.map(asset => <img className={defaultImage === asset.id ? 'isset-video-default-image' : undefined} src={`${asset.url}?width=150&height=80`} key={`asset-${asset.id}`} onClick={() => this.setDefaultImage(asset.id)} />)}
+                        {!this.hasCustomImage(assets) && <div className="isset-video-add-default-still" onClick={this.uploadCustomImage}>
+                            <span className="dashicons dashicons-plus" />
+                        </div>}
+                        {assets.map(asset => <div className="video-block-relative video-publisher-inline-block">
+                            {asset.id === 0 && <span className="dashicons dashicons-update-alt still-replace" onClick={this.uploadCustomImage} />}
+                            <img className={defaultImage === asset.id ? 'isset-video-default-image' : undefined} src={`${asset.url}?width=150&height=80`} key={`asset-${asset.id}`} onClick={() => this.setDefaultImage(asset.id)} />
+                        </div>)}
                     </div>
                 </div>
 
@@ -168,7 +252,12 @@ class AdminVideoDetails extends React.Component {
     };
 
     render() {
+        const {settingCustomImage} = this.state;
         const detailsContainer = document.getElementById('issetVideoOverlay');
+
+        if (settingCustomImage) {
+            return ReactDOM.createPortal(this.renderUploadCustomStill(), detailsContainer);
+        }
 
         return ReactDOM.createPortal(this.renderDetails(), detailsContainer);
     }
